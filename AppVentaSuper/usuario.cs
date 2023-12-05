9a1b72735ec2beffaf10c58b236c74e5b8907149
CarlosDeveloper21 <151039363+CarlosDeveloper21@users.noreply.github.com>
2023-12-05 08:59:36 -0400

    Update usuario.cs

--- AppVentaSuper/usuario.cs
+++ AppVentaSuper/usuario.cs
@@ -23,15 +23,11 @@ namespace AppVentaSuper
     
         public int idusuario { get; set; }
         public int idrol { get; set; }
-        public string nombre { get; set; }
-        public string tipo_documento { get; set; }
-        public string num_documento { get; set; }
-        public string direccion { get; set; }
-        public string telefono { get; set; }
         public string correo { get; set; }
         public string clave { get; set; }
         public System.DateTime fecha { get; set; }
         public Nullable<bool> estado { get; set; }
+        public string confirmarClave {get;set;}
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<compra> compra { get; set; }
